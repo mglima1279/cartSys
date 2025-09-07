78c9656942b2eb0242e6651589ed2f47dcf80b1c
Miguel Lima <mglima1279@gmail.com>
2025-09-06 21:42:20 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,107 @@
+import { item, cartEl } from "./module.js";
+
+const itemContainer = document.querySelector(".itemsContainer");
+const cart = document.querySelector(".cart");
+const result = document.getElementById("result")
+let cartItems = []
+let cartNames = []
+let cartsum = 0
+
+loadData();
+
+fetch("./js/db.json")
+.then(res => res.json())
+.then(db => {
+    db.forEach(el=>{
+        const newItem = new item(el.nam, el.imgNam, el.price);
+    
+        newItem.htmlEl.addEventListener("click", ()=>{
+            result.textContent = ""
+            if(!cartNames.includes(newItem.name)){
+                cartNames.push(newItem.name)
+                const newCartItem = new cartEl(newItem.name, newItem.imgNam, newItem.price)
+                cartItems.push(newCartItem)
+    
+                newCartItem.rightArrow.addEventListener("click", ()=>{
+                    addTo(newCartItem)
+                })
+    
+                newCartItem.leftArrow.addEventListener("click", ()=>{
+                    removeFrom(newCartItem)
+                })
+    
+                cart.appendChild(newCartItem.htmlEl)
+                saveData();
+            }
+            addTo(cartItems[cartNames.indexOf(newItem.name)])
+         })
+         itemContainer.appendChild(newItem.htmlEl);
+    });
+});
+
+
+
+function addTo(item){
+    cartsum+=parseFloat(item.price)
+    result.textContent=cartsum.toFixed(2)
+
+    item.amountValue++;
+    item.amount.textContent = item.amountValue
+    saveData()
+}
+
+function removeFrom(item){
+    cartsum-=parseFloat(item.price)
+    result.textContent=cartsum.toFixed(2)
+
+    if(item.amountValue > 1){
+        item.amountValue--;
+        item.amount.textContent = item.amountValue
+    } else{
+        let locIndex = cartItems.indexOf(item);
+
+        cartItems.splice(locIndex, 1);
+        cartNames.splice(locIndex, 1);
+
+        cart.removeChild(item.htmlEl)
+        item = null        
+    }
+    saveData();
+}
+
+function saveData() {
+    localStorage.clear();
+
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+    localStorage.setItem("cartNames", JSON.stringify(cartNames))
+}
+
+function loadData() {
+    const savedData = JSON.parse(localStorage.getItem("cartItems"))
+    if (!savedData) return;
+
+    savedData.forEach(el => {
+        const newLoadCartItem = new cartEl(el.nameValue, el.imgNam, el.price);
+        newLoadCartItem.amountValue = el.amountValue;
+        newLoadCartItem.amount.textContent = newLoadCartItem.amountValue;
+
+        cartsum += parseFloat(el.price)*el.amountValue
+        result.textContent = cartsum.toFixed(2)
+        cartItems.push(newLoadCartItem)
+
+        newLoadCartItem.rightArrow.addEventListener("click", ()=>{
+            addTo(newLoadCartItem)
+        })
+
+        newLoadCartItem.leftArrow.addEventListener("click", ()=>{
+            removeFrom(newLoadCartItem)
+        })
+
+        cart.appendChild(newLoadCartItem.htmlEl)
+    })
+    const savedNames = JSON.parse(localStorage.getItem("cartNames"));
+
+    savedNames.forEach(el => {
+        cartNames.push(el)
+    })
+}
---
+++ js/module.js
@@ -0,0 +1,69 @@
+export class item{
+  constructor(nam, imgNam, price){
+    this.name = nam;
+    this.price = price.toFixed(2);
+    this.imgNam = imgNam;
+
+    this.htmlEl = document.createElement("div");  
+    this.img = document.createElement("img");
+    this.img.src = `./styles/imgs/${this.imgNam}`
+    this.nameSlot = document.createElement("span");
+    this.priceSlot = document.createElement("span");
+
+    const slotsWrappers = document.createElement("div");
+
+
+    this.nameSlot.textContent = this.name;
+    this.priceSlot.textContent = this.price;
+
+    slotsWrappers.appendChild(this.nameSlot);
+    slotsWrappers.innerHTML+="|X$";
+    slotsWrappers.appendChild(this.priceSlot);
+    this.htmlEl.appendChild(this.img);
+    this.htmlEl.appendChild(slotsWrappers);
+
+    this.htmlEl.classList.add("item");
+  }
+}
+
+export class cartEl{
+  constructor(nam, imgNam, price){
+    this.price = price;
+    this.nameValue = nam;
+    this.imgNam = imgNam;
+
+    this.htmlEl = document.createElement("div");
+    this.htmlEl.classList.add("cartItem");
+    
+    this.leftArrow = document.createElement("i");
+    this.leftArrow.classList.add("fa-solid");
+    this.leftArrow.classList.add("fa-chevron-left");
+    
+    this.rightArrow = document.createElement("i");
+    this.rightArrow.classList.add("fa-solid");
+    this.rightArrow.classList.add("fa-chevron-right");
+
+    this.amount = document.createElement("span");
+    this.amountValue = 0;
+    this.amount.textContent = this.amountValue;
+
+    const numberHandlers = document.createElement("div");
+
+    numberHandlers.appendChild(this.leftArrow);
+    numberHandlers.appendChild(this.amount);
+    numberHandlers.appendChild(this.rightArrow);
+    this.htmlEl.appendChild(numberHandlers);
+
+    this.name = document.createElement("span");
+    this.name.textContent = nam;
+    this.htmlEl.appendChild(this.name);
+
+    this.priceSpan = document.createElement("span");
+    this.priceSpan.innerHTML = `X$${this.price}`;
+    this.htmlEl.appendChild(this.priceSpan);
+
+    const newImg = document.createElement("img");
+    newImg.src = `./styles/imgs/${this.imgNam}`;
+    this.htmlEl.appendChild(newImg);
+  }
+}
